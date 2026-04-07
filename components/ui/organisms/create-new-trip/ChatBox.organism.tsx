3d18282c6c064c721534f2ea3d73debfd1408385
amine9005 <amamselmitrader@gmail.com>
2026-04-07 23:46:35 +0100

    updates

---
+++ components/ui/organisms/create-new-trip/ChatBox.organism.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatBoxOrganism = () => {
+  return <div>ChatBoxOrganism</div>;
+};
+
+export default ChatBoxOrganism;
--- components/ui/organisms/create-new-trip/CreateNewTrip.organism.tsx
+++ components/ui/organisms/create-new-trip/CreateNewTrip.organism.tsx
@@ -1,5 +1,14 @@
+import ChatBoxOrganism from "./ChatBox.organism";
+
 const CreateNewTripOrganism = () => {
-  return <div>CreateNewTripOrganism</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div>
+        <ChatBoxOrganism />
+      </div>
+      <div>Map</div>
+    </div>
+  );
 };
 
 export default CreateNewTripOrganism;
